c0a20fe472319e8fae640f0afd2f74f965c39d6c
Vrushali A Poojary <vrushaliash04@gmail.com>
2025-08-15 21:12:33 +0530

    learning

--- lib/main.dart
+++ lib/main.dart
@@ -45,6 +45,6 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           title: Text(widget.title),
         ),
-        body: Text("hello"));
+        body: Text("hello world"));
   }
 }
