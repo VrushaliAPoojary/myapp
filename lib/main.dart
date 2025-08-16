9f58cf27bb22a58c204369c3c17ead4f4a718873
Vrushali A Poojary <vrushaliash04@gmail.com>
2025-08-16 13:50:38 +0530

    Row

--- lib/main.dart
+++ lib/main.dart
@@ -45,18 +45,55 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           title: Text(widget.title),
         ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Text(
-              'hii',
-              style: TextStyle(fontSize: 25),
-            ),
-            Text('hii'),
-            Text('hii'),
-            Text('hii'),
-            ElevatedButton(onPressed: () {}, child: Text('click'))
-          ],
+        body: Container(
+          height: 300,
+          width: 300,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'A',
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  Text(
+                    'B',
+                    style: TextStyle(fontSize: 28),
+                  ),
+                  Text(
+                    'C',
+                    style: TextStyle(fontSize: 28),
+                  ),
+                  Text(
+                    'D',
+                    style: TextStyle(fontSize: 28),
+                  ),
+                  ElevatedButton(onPressed: () {}, child: Text('click'))
+                ],
+              ),
+              Text(
+                'A',
+                style: TextStyle(fontSize: 25),
+              ),
+              Text(
+                'B',
+                style: TextStyle(fontSize: 28),
+              ),
+              Text(
+                'C',
+                style: TextStyle(fontSize: 28),
+              ),
+              Text(
+                'D',
+                style: TextStyle(fontSize: 28),
+              ),
+              ElevatedButton(onPressed: () {}, child: Text('click'))
+            ],
+          ),
         ));
   }
 }
