fd6bacba94bfea12b18e40ac5b4ac3eac39aabe7
Vrushali A Poojary <vrushaliash04@gmail.com>
2025-08-16 12:53:54 +0530

    Row

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           title: Text(widget.title),
         ),
-        body: Column(
+        body: Row(
           children: [
             Text(
               'hii',
