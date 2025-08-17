27c4ba1e6cd5483277b897cd8d35021b0eadabf1
Vrushali A Poojary <vrushaliash04@gmail.com>
2025-08-17 23:01:59 +0530

    learning

--- lib/main.dart
+++ lib/main.dart
@@ -20,28 +20,12 @@ class DashBoardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Dashboard"),
-      ),
-      body: Center(
-        child: CircleAvatar(
-          child: Container(
-            width: 60,
-            height: 60,
-            child: Column(
-              children: [
-                Container(
-                    width: 40,
-                    height: 30,
-                    child: Image.asset('assets/images/img_1.png')),
-                Text('Appu')
-              ],
-            ),
-          ),
-          backgroundColor: Colors.pink,
-          maxRadius: 70,
+        appBar: AppBar(
+          title: Text("Dashboard"),
         ),
-      ),
-    );
+        body: Text(
+          "HELLO BRO WASSUP....hope you doing well",
+          style: TextStyle(fontFamily: 'Fontg'),
+        ));
   }
 }
