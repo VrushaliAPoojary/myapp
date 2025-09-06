3ac26943fe46546f3626506fcae47b7b5730fce6
Vrushali A Poojary <vrushaliash04@gmail.com>
2025-09-06 23:15:56 +0530

    learning

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,19 @@ class LearnFlutter extends StatelessWidget {
       title: "flutter",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          primarySwatch: Colors.pink,
-          textTheme: TextTheme(
-              headlineMedium:
-                  TextStyle(fontSize: 30, fontWeight: FontWeight.w900),
-              headlineSmall: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w300,
-                  fontStyle: FontStyle.italic))),
+        primarySwatch: Colors.pink,
+        textTheme: const TextTheme(
+          headlineMedium: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.w900,
+          ),
+          headlineSmall: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.w300,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+      ),
       home: const DashBoardScreen(),
     );
   }
@@ -32,20 +37,40 @@ class DashBoardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Dashboard"),
-        ),
-        body: Center(
-          child: Card(
-              shadowColor: Colors.pink,
-              elevation: 25,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "hisiis",
-                  style: TextStyle(fontSize: 33),
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+      ),
+      body: Center(
+        child: Container(
+          width: 300,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.pink, width: 2),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.pink, width: 1),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
                 ),
-              )),
-        ));
+              ),
+              const SizedBox(height: 11),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.pink),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
